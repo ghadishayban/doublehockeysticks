4864ac9bfdc50289d27978ddad7505c09ee4d0fc
Ghadi Shayban <gshayban@gmail.com>
2011-12-04 12:23:22 -0500

    Added docs, typehints

--- src/parser/parse.clj
+++ src/parser/parse.clj
@@ -4,28 +4,37 @@
 
 ;;(set! *warn-on-reflection* true)
 
+;; TODO: Test ClojureScript version.
+;; Does that need a special exception handler?
+;; Need to write a StringReader for that.
+
 (def defaultBufSize 3)
 
 ;; ASCII codes of character delimiters
 (def ASCII_VT (char 11))
 (def ASCII_FS (char 28))
-(def ASCII_CR (char 13))
+(def ASCII_CR \return)
 (def ASCII_LF (char 10))
 
-;; HL7 Messaging v2.x segment delimite
+;; HL7 Messaging v2.x segment delimiter
 (def SEGMENT-DELIMITER ASCII_CR)
-
-(defn test-rdr [s]
-  (PushbackReader. (StringReader. s)))
 
 (defprotocol Stream
   (read [x])
   (unread [x]))
 
-(deftype Reader [rdr buf pushCnt bufSize pos]
+(deftype Reader 
+  ^{:doc "This type is PushbackReader, except you don't have to tell it
+  what to push back.  Also, it returns chars, not ints.
+  rdr is the underlying Reader, buf is a primitive char array,
+  pushCnt tracks how many chars have been pushed back into the
+  stream, bufSize is the size of the primitive array, and pos
+  is the position within buf that should be read."}
+  [^PushbackReader rdr ^chars buf pushCnt bufSize pos]
   Stream
   (read [_]
         (if (> @pushCnt 0)
+          ;; pos can be greater than bufSize, so mod it.
           (let [ch (aget buf (mod (- @pos @pushCnt) bufSize))]
             (do (swap! pushCnt dec)
               ch))
@@ -44,7 +53,10 @@
            (throw (Exception. "Too much unreading!")))
          nil))
 
-(defn parser [s]
+(defn parser 
+  "Constructs a Reader from a String. 
+  This method is for Java." 
+  [s]
   (Reader. (PushbackReader. (StringReader. s))
            (char-array defaultBufSize)
            (atom 0)
@@ -66,8 +78,6 @@
                        escape
                        subcomponent])
 
-(def segdelim \return)
-
 (defn read-delimiters [r]
   (loop [ch (read r)
          delim {} 
@@ -77,7 +87,7 @@
         (nil? ch)
         (throw (Exception. "EOF in delimiters"))
 
-        (= ch segdelim)
+        (= ch SEGMENT-DELIMITER)
         (throw (Exception. "Segment ended in delimiters"))
 
         (= 3 pos)
@@ -115,7 +125,7 @@
         (= 8 pos)
           (if (= ch (:field delim))
             (do (unread r) (map->Delimiters delim))
-            (throw (Exception. "No field delim after delimiters!")))
+            (throw (Exception. "No field delim terminating delimiters.")))
         
         :else
         (recur (read r)
@@ -123,26 +133,32 @@
                (inc pos)
                (conj acc ch)))))
 
+
 (defrecord Message [delimiters segments])
 (defrecord Segment [id fields])
 (defrecord Field [components])
 (defrecord Component [subcomponents])
 (defrecord RepeatingField [fields])
 
-(defn read-escaped [r {:keys [escape]}]
+(defn read-escaped [r {:keys [escape] :as delim}]
   (loop [acc [] ch (read r)]
     (cond
       (nil? ch)
       (throw (Exception. "EOF during escape sequence."))
 
       (= escape ch)
-      ;; ADD ESCAPE SEQUENCE TRANSLATION HERE
+      ;; TODO: Add escape sequence translation here,
+      ;; pass delimiters to translation helper.
       (apply str acc)
 
       :else
       (recur (conj acc ch) (read r)))))
 
-(defn read-text [r {:keys [escape field component repeating subcomponent]:as delim}]
+(defn read-text [r {:keys [escape 
+                           field 
+                           component 
+                           repeating 
+                           subcomponent] :as delim}]
   (loop [acc [] ch (read r)]
     (cond
       (nil? ch)
@@ -175,7 +191,9 @@
       (Component. acc)
 
       (= ch subcomponent)
-      (recur (conj acc (read-text r delim)) 
+      (recur (conj acc (read-text r delim)) ;; just add the subcomponent
+                                            ;; to the vector, no need for
+                                            ;; and additional structure
              (read r))
 
       (= ch component)
@@ -194,14 +212,16 @@
                             component 
                             repeating 
                             subcomponent] :as delim}]
+  ;; field-acc helps to handle repeating fields.
+  ;; acc is for the components within a single field.
   (loop [acc [] ch (read r) field-acc []]
     (cond
+
+      ;; if the field-acc isn't empty, make a repeating field
       (nil? ch)
       (if (seq field-acc)
         (RepeatingField. (conj field-acc (Field. acc)))
         (Field. acc))
-
-      ;;(= ch repeating)
 
       (or (= ch field) (= ch SEGMENT-DELIMITER))
       (do (unread r) 
@@ -209,6 +229,8 @@
           (RepeatingField. (conj field-acc (Field. acc)))
           (Field. acc)))
 
+      ;; When the field repeats, Empty out acc into a new field,
+      ;; and place it in field-acc.
       (= ch repeating)
       (recur [] 
              (read r) 
@@ -220,7 +242,11 @@
                (read r) 
                field-acc)))))
 
-(defn read-segment-fields [r {:keys [escape field component repeating subcomponent] :as delim}]
+(defn read-segment-fields [r {:keys [escape
+                                     field 
+                                     component 
+                                     repeating 
+                                     subcomponent] :as delim}]
   (loop [acc [] ch (read r)]
     (cond
       (nil? ch)
@@ -232,17 +258,32 @@
       (= ch field)
         (recur (conj acc (read-field r delim)) (read r)))))
 
-(defn read-segment-header [r {:keys [escape field component repeating subcomponent] :as delim}]
-  (let [seg-ID (read-text r delim)]
+(defn read-segment-header
+  "This returns the three character label on the Segment.
+  It calls read-text, so it can potentially handle escape
+  sequences.  Not a good idea."
+  [r {:keys [escape 
+             field 
+             component 
+             repeating 
+             subcomponent] :as delim}]
+  (let [seg-ID (read-text r delim)]  ;; FIXME: read-text can parse escapes
     (if (or (nil? seg-ID) (not= 3 (count seg-ID)))
       (throw (Exception. "Bad segment header"))
       seg-ID)))
 
-(defn read-segment [r {:keys [escape field component repeating subcomponent] :as delim}]
+(defn read-segment 
+  [r {:keys [escape 
+             field 
+             component 
+             repeating 
+             subcomponent] :as delim}]
   (Segment. (read-segment-header r delim)
             (read-segment-fields r delim)))
 
-(defn read-message [r] 
+(defn read-message
+  "Parses the HL7 message.
+  Parameter r must implement the Reader protocol." [r] 
   (let [delim (read-delimiters r)
         msh-fields (read-segment-fields r delim)]
       (loop [acc [(Segment. "MSH" msh-fields )]
@@ -252,7 +293,8 @@
           (Message. delim
                     acc)
 
+          ;; FIXME: Also terminate reading upon encountering End Block character(s)
           (= ch SEGMENT-DELIMITER)
           (recur (conj acc (read-segment r delim)) (read r))))))
 
-(def r (test-msg))
+(comment (def r (test-msg)))
