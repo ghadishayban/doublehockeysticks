750aa57ebdbc1aa5c2965968c44e7defb0b258de
Ghadi Shayban <gshayban@gmail.com>
2012-09-20 15:10:52 -0400

    compute Luhn algorithm checksum + tests

---
+++ src/parser/luhn.clj
@@ -0,0 +1,34 @@
+(ns parser.luhn)
+
+(defn luhn [s]
+  (let [deltas [0 1 2 3 4 -4 -3 -2 -1 0]
+        digits (map #(Character/digit % 10) s)]
+    (loop [acc 0 d digits position 0]
+      (if (seq d)
+        (recur (+ acc (if (odd? position)
+                          (+ (first d) (get deltas (first d)))
+                          (first d)))
+               (rest d)
+               (inc position))
+        (let [r (mod acc 10)]
+          (case r
+            0 0
+            (- 10 r)))))))
+
+(defn luhn [s]
+  (let [deltas [0 1 2 3 4 -4 -3 -2 -1 0]
+    digits (map #(Character/digit % 10) s)]
+    (loop [acc 0 d digits odd? false]
+      (if (seq d)
+        (recur (+ acc (if odd?
+                          (+ (first d) (get deltas (first d)))
+                          (first d)))
+               (rest d)
+               (not odd?))
+        (let [r (mod acc 10)]
+          (case r
+            0 0
+            (- 10 r)))))))
+
+
+
