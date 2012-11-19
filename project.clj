7061e1b8b85597f50b79064b2d665bfbdf7887ea
Ghadi Shayban <gshayban@gmail.com>
2012-11-18 22:57:10 -0500

    point to new main ns

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject parser "1.0.0-SNAPSHOT"
+(defproject hl7v2 "1.0.0-SNAPSHOT"
   :url "http://github.com/ghadishayban/doublehockeysticks"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "FIXME: write description"
-  :main ^:skip-aot parser.parse
+  :main ^:skip-aot com.shayban.hl7v2.parse
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[criterium "0.3.0-SNAPSHOT"]]}})
